10a0fbc40631f620c45606bb55e6901e622f3a01
mpkcb21 <mpudukottaikrishnara@ucsd.edu>
2026-04-23 15:09:22 -0700

    Update global.js

--- global.js
+++ global.js
@@ -62,3 +62,18 @@ select.addEventListener('input', function (event) {
   document.documentElement.style.setProperty('color-scheme', event.target.value);
   localStorage.colorScheme = event.target.value;
 });
+export async function fetchJSON(url) {
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data;
+
+  } catch (error) {
+    console.error('Error fetching or parsing JSON data:', error);
+  }
+}
