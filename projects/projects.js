b65509d5eeb7be3ba7d832014e34131c7ed13dc1
mpkcb21 <mpudukottaikrishnara@ucsd.edu>
2026-04-30 10:13:13 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -9,10 +9,16 @@ const projectsTitle = document.querySelector('.projects-title');
 projectsTitle.textContent = `Projects (${projects.length})`;
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 
-let data = [1, 2, 3, 4, 5, 5];
+let data = [
+  { value: 1, label: 'apples' },
+  { value: 2, label: 'oranges' },
+  { value: 3, label: 'mangos' },
+  { value: 4, label: 'pears' },
+  { value: 5, label: 'limes' },
+  { value: 5, label: 'cherries' },
+];
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
-let sliceGenerator = d3.pie();
-let arcData = sliceGenerator(data);
+let sliceGenerator = d3.pie().value((d) => d.value);let arcData = sliceGenerator(data);
 let arcs = arcData.map((d) => arcGenerator(d));
 
 arcs.forEach((arc, idx) => {
