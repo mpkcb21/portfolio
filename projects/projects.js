4a470dd382a38f8ced71ea888d827973864137dc
mpkcb21 <mpudukottaikrishnara@ucsd.edu>
2026-04-30 09:59:48 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -9,9 +9,15 @@ const projectsTitle = document.querySelector('.projects-title');
 projectsTitle.textContent = `Projects (${projects.length})`;
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 
-let arc = arcGenerator({
-  startAngle: 0,
-  endAngle: 2 * Math.PI,
-});
+let data = [1, 2, 3, 4, 5, 5];
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
+let sliceGenerator = d3.pie();
+let arcData = sliceGenerator(data);
+let arcs = arcData.map((d) => arcGenerator(d));
 
-d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
+arcs.forEach((arc, idx) => {
+  d3.select('svg')
+    .append('path')
+    .attr('d', arc)
+    .attr('fill', colors(idx));
+});
