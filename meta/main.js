77f42c00bced634c7a5b8a5b3e53bebec3b0e9da
mpkcb21 <mpudukottaikrishnara@ucsd.edu>
2026-05-08 23:13:42 -0700

    Update main.js

--- meta/main.js
+++ meta/main.js
@@ -13,3 +13,35 @@ async function loadData() {
 }
 
 let data = await loadData();
+function processCommits(data) {
+  return d3
+    .groups(data, (d) => d.commit)
+    .map(([commit, lines]) => {
+      let first = lines[0];
+      let { author, date, time, timezone, datetime } = first;
+      let ret = {
+        id: commit,
+        url: 'https://github.com/mpkcb21/portfolio/commit/' + commit,
+        author,
+        date,
+        time,
+        timezone,
+        datetime,
+        hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+        totalLines: lines.length,
+      };
+
+      Object.defineProperty(ret, 'lines', {
+        value: lines,
+        configurable: true,
+        writable: true,
+        enumerable: false,
+      });
+
+      return ret;
+    });
+}
+
+let data = await loadData();
+let commits = processCommits(data);
+console.log(commits);
